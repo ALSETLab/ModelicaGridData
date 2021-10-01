9a44c1897bbad4dc579cff9d741449141319f38e
Sergio Dorado-Rojas <sergio.dorado.rojas@gmail.com>
2021-10-01 16:14:55 -0400

    Fixed problem with path

--- utils/extract_data/extract_data.py
+++ utils/extract_data/extract_data.py
@@ -4,10 +4,23 @@ import sdf
 import pandas as pd
 
 def extract_data(model, path, working_directory):
-'''
+    '''
+    EXTRACT_DATA
 
+    INPUTS:
+    - `model`: model for which the simulation data will be extracted
+    and processed. Default: `IEEE14`.
+    - `path`: path where the output `*.csv` files will be saved
+    -  `working_directory`: directory where the simulation outputs (i.e.,
+    `*.mat` files`) are located for the given model
 
-'''
+    OUTPUTS:
+    Writes `*.csv` files with the specified variables by the user in
+    `./data/<path>`
+
+    LAST MODIFICATION DATE:
+    10/01/2021 BY SADR
+    '''
 
     # Model name in simulations
     _model_name = f"{model}_Base_Case"
