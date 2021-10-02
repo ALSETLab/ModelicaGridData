403f289486abc467dbfc6713886e111bcee8ca25
Sergio Dorado-Rojas <sergio.dorado.rojas@gmail.com>
2021-10-02 19:22:06 -0400

    Fixed issue with power flow scenarios

--- utils/automated_simulation/om_simulation.py
+++ utils/automated_simulation/om_simulation.py
@@ -185,8 +185,8 @@ def om_simulation(pf_list, scenarios, data_path, sim_params, n_proc):
                 + f",tolerance={_tolerance},method=\"{_method}\",numberOfIntervals={_numberOfIntervals}"
             _simOptions = _simSettings + f",fileNamePrefix=\"{_model_package}_lin0_{counter}\""
 
-            ss = omc.sendExpression(f"Modelica_LinearSystems2.Utilities.Import.linearize2(\"{_model_package}\")")
-            print(ss)
+            res = omc.sendExpression(f"linearize({_model_package}.{_model_name},simflags=\"-overrideFile=trip_line.txt\")")
+            print(res,_model_package,_model_name)
             return
 
             # res = omc.sendExpression(f"linearize({_model_package}.{_model_name},{_simOptions},simflags=\"-overrideFile=trip_line.txt\")")
