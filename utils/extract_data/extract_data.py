3af08784b27932a9adf67ed93567f2ffe08e48be
Sergio A. Dorado-Rojas <sergio.dorado@outlook.de>
2021-10-14 17:45:25 -0400

    Added list of generator components in OpenIPSL 1.5.0 and 2.0.0

--- utils/extract_data/extract_data.py
+++ utils/extract_data/extract_data.py
@@ -232,7 +232,7 @@ def extract_data(tool, model, version, path, working_directory):
                             elif res_format == 'polar':
                                 pass
                     elif extract == 'generators':
-                        print(resData[_generators[0]].__dict__)
+                        print(resData[_generators[0]].__dict__.keys())
 
     ##########################################################
     # Concatenating all results in an `*.hdf5` file
